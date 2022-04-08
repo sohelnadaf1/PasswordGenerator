99c1289fdb4483cbaba3a57d6cb9f3006bc99422
sohelnadaf1 <sohel.nadaf@jkteck.com>
2022-04-08 17:12:29 +0530

    angularProject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PasswordGenerator';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includesymbols = false;
+  password = ''
+
+  // getting the input from user and converting into number 
+  getLength(value:any)
+  {
+    const parsedValue = parseInt(value);
+    
+    if(!isNaN(parsedValue))
+    {
+      this.length = parsedValue;
+    }
+    console.log(this.length)
+  }
+
+  changeLetter(){
+    this.includeLetters = !this.includeLetters;
+
+  }
+
+  changeNumber()
+  {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  changeSymbol()
+  {
+    this.includesymbols = !this.includesymbols;
+  }
+  buttonClick()
+  {
+    const  allLetters = 'qwertyuioplkjhgfdsazxcvbnm';
+    const allSymbols = '!@#$%^&*()_';
+    const  allNumbers = '0123456789';
+
+   let validChar = '';
+
+   if(this.includeLetters){
+     validChar += allLetters;
+   }
+   if(this.includeNumbers){
+     validChar += allNumbers;
+   }
+   if(this.includesymbols){
+     validChar += allSymbols;
+   }
+
+   let gereratedpassword = '';
+   for(let i = 0; i<this.length; i++)
+   {
+     const index = Math.floor(Math.random() * validChar.length);
+     console.log(index);
+     gereratedpassword += validChar[index];
+     
+   }
+
+   this.password = gereratedpassword;
+   
+  }
 }
